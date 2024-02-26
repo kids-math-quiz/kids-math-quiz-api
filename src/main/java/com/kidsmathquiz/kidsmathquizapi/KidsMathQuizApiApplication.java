f5db78721dd38a8d209cd679bfbcf1784ad68293
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2024-02-26 10:57:41 -0300

    initial commit, project spring initializer

---
+++ src/main/java/com/kidsmathquiz/kidsmathquizapi/KidsMathQuizApiApplication.java
@@ -0,0 +1,13 @@
+package com.kidsmathquiz.kidsmathquizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsMathQuizApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsMathQuizApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kidsmathquiz/kidsmathquizapi/KidsMathQuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidsmathquiz.kidsmathquizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsMathQuizApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
